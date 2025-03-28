2d8733045ea8fd75f1d9eba568bb66edd6826422
Washer <fluidwasher@gmail.com>
2025-03-28 10:20:28 -0700

    fewer

--- fix.ts
+++ fix.ts
@@ -7,11 +7,14 @@ function replaceAll(str: string, find: string, replace: string) {
 async function fix(filename: string) {
     const contents = fs.readFileSync(filename, 'utf8');
 
-    let fixed = replaceAll(contents, 'P E N D O W N', 'PEN_DOWN');
-    fixed = replaceAll(fixed, 'PENUP', 'PEN_UP ');
-    fixed = replaceAll(fixed, 'P E N U P', 'PEN_UP');
+    let fixed = replaceAll(contents, 'P E N D O W N', '\nPEN_DOWN');
+    fixed = replaceAll(fixed, 'PENUP', 'PEN_UP\n');
+    fixed = replaceAll(fixed, 'P E N U P', 'PEN_UP\n');
     fixed = replaceAll(fixed, 'P R I N T S T A R T', 'PRINT_START');
-    fixed = replaceAll(fixed, 'P R I N T E N D', 'PRINT_END');
+    fixed = replaceAll(fixed, 'P R I N T E N D', '\nPRINT_END');
+
+    const index = fixed.lastIndexOf('PEN_UP')
+    fixed = fixed.slice(0, index) + '\n' + fixed.slice(index + 1);
 
     const newFilename = filename.split('.')
 
